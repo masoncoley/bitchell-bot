45e0c19dfe7279cbf14539abd1a2ca0bf424f9c8
masoncoley <masoncoley@gmail.com>
2017-10-13 02:26:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,5 +20,18 @@ client.on('message', message => {
    })
  }});
 
+client.on('message', message => {
+   if (message.content === 'brobot') {
+      message.channel.send({embed: {
+          title: "This is a BroCorp Discord bot",
+          description: "I'm testing this bot out to be awesome as fuck and do some cool shit. 100% coded using Node JS on 24/7 Heroku",
+          url: "https://brocorp.io/",
+          color: 15277667,
+          thumbnail: {
+          url: "http://masoncoley.com/wp-content/uploads/2017/05/10012484_816736341675454_1800625882_n.png"
+                     }}
+   })
+ }});
+
 // THIS MUST BE THIS WAY
 client.login(process.env.BOT_TOKEN);
