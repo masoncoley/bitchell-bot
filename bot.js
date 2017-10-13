0be8a200e2e0a9e881bb1577ce0c6655886de51e
masoncoley <masoncoley@gmail.com>
2017-10-13 00:59:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,12 +17,5 @@ client.on('message', message => {
    }
 });
 
-client.on('message', message => {
-   if (message.content === 'testing') {
-      message.reply({embed: {
-         color: 3447003,
-         description: "A very simple Embed!"
-}});
-
 // THIS MUST BE THIS WAY
 client.login(process.env.BOT_TOKEN);
